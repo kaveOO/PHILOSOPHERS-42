f9950cdc2d169d093528d95f23ab8100bca75d8d
kaveO <alban.billiette@gmail.com>
2025-01-08 04:33:11 +0100

    + | Added some data and todo for tomorrow.

--- philo/src/core/philo.c
+++ philo/src/core/philo.c
@@ -6,18 +6,20 @@
 /*   By: albillie <albillie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/07 19:49:44 by albillie          #+#    #+#             */
-/*   Updated: 2025/01/08 02:55:52 by albillie         ###   ########.fr       */
+/*   Updated: 2025/01/08 04:11:26 by albillie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+
+// TODO Just create routine of each philo with mutex lock and unlock
 
 int	main(int ac, char **av)
 {
 	t_philo	*philo;
 
 	(void) av;
-	if (ac < 5)
+	if (ac < 5 || ac > 6)
 		format();
 	philo = init_philo_struct();
 }
