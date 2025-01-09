b5cc16703dd544b7b0c439306dd8cff796f6a38b
kaveO <alban.billiette@gmail.com>
2025-01-09 02:30:07 +0100

    + | Mooves some files and added libscam folder with some lib functions

--- philo/src/handling/handling.c
+++ philo/src/handling/handling.c
@@ -6,7 +6,7 @@
 /*   By: kaveo <kaveo@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/08 01:51:11 by albillie          #+#    #+#             */
-/*   Updated: 2025/01/09 00:02:40 by kaveo            ###   ########.fr       */
+/*   Updated: 2025/01/09 02:26:52 by kaveo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,4 +21,17 @@ void	format()
 	printf("int ->		<time_to_sleep>		(in milliseconds)\n");
 	printf("int ->		<num_time_to_eat>	(optional)\n\n");
 	exit(0);
+}
+
+void	args_error(char *str)
+{
+	int	i;
+	i = 0;
+
+	write(2, "philoArgs -> ", 14);
+	while (str[i])
+	{
+		write(2, &str[i], 1);
+		i++;
+	}
 }
---
+++ philo/src/utils/parsing.c
@@ -0,0 +1,99 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parsing.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kaveo <kaveo@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/09 01:05:48 by kaveo             #+#    #+#             */
+/*   Updated: 2025/01/09 01:07:05 by kaveo            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+bool	check_chars(char **av)
+{
+	int	i;
+	int	j;
+
+	i = 1;
+	while (av[i])
+	{
+		j = 0;
+		while (av[i][j])
+		{
+			if (!ft_isdigit(av[i][j]))
+			{
+				args_error("Expecting Digits\n");
+				return (false);
+			}
+			j++;
+		}
+		i++;
+	}
+	return (true);
+}
+
+bool	check_limits(char **av)
+{
+	int	i;
+
+	i = 1;
+	while (av[i])
+	{
+		if (ft_atol(av[i]) > 2147483647 || ft_atol(av[i]) < -2147483648)
+		{
+			args_error("Check for MAX_INT or MIN_INT\n");
+			return (false);
+		}
+		i++;
+	}
+	return (true);
+}
+
+bool	check_len(char **av)
+{
+	int		i;
+	size_t	j;
+
+	i = 1;
+	while (av[i])
+	{
+		j = 0;
+		while (ft_isdigit(av[i][j]))
+			j++;
+		if (j > ft_strlen("9223372036854775807"))
+		{
+			args_error("Check for LONG_MAX or LONG_MIN\n");
+			return (false);
+		}
+		i++;
+	}
+	return (true);
+}
+
+bool	check_values(char **av)
+{
+	if (av[1] && ft_atol(av[1]) < 1)
+	{
+		return (args_error("Philos count is less than 1\n"), false);
+	}
+	else if (av[2] && ft_atol(av[2]) < 1)
+	{
+		return (args_error("Time to die is less than 1\n"), false);
+	}
+	else if (av[3] && ft_atol(av[3]) < 1)
+	{
+		return (args_error("Time to eat is less than 1\n"), false);
+	}
+	else if (av[4] && ft_atol(av[4]) < 1)
+	{
+		return (args_error("Time to sleep is less than 1\n"), false);
+	}
+	else if (av[5] && ft_atol(av[5]) < 1)
+	{
+		return (args_error("Number of time to eat is less than 1\n"), false);
+	}
+	return (true);
+}
